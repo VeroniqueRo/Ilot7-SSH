4773ac6d27a81651f01ac93e5ac71f6eabb915f6
Veronique Rouault <veronique.rouault@Digital-Grenoble.local>
2018-01-11 15:15:48 +0100

    Mise sous git de MachineACafe pour serveur Debian

---
+++ MachineACafe/assets/_php/fonctions.php
@@ -0,0 +1,102 @@
+<?php
+
+/* Déclaration des variables */
+
+$date = date("l d F Y"); // Déclaration d'une variable $date qui prend pour valeur la fonction date avec les paramètres le jour (nom + numéro) le mois et l'année
+$heure = date("H"); // Déclaration d'une variable $heure qui prend pour valeur la fonction date avec le paramètre Heure
+$minutes  = date("i"); // Déclaration d'une variable $minutes qui prend pour valeur la fonction date avec le paramètre minutes
+$argentInsere = 0; // Déclaration de la variable $argentInsere qui prend pour valeur 0
+$messageAttente = "Vous voulez un café ou bien ?";
+
+/* Déclaration des fonctions : Version avec BDD EXTERNE */
+
+// Fonction de connection à la BDD
+function connection(){
+    //teste les infos d'accès à la base de données
+    try 
+    {
+      // connexion à MySQL
+      $bdd = new PDO('mysql:host=localhost;dbname=machineacafe;charset=utf8', 'Groot66', 'TuTeCalmes!');  
+    }
+    catch (Exception $e)
+    {
+      // en cas d'erreur, affiche un message et s'arrête
+      die('Erreur : '.$e->getMessage());
+    }
+    return $bdd;
+  }
+
+$mabdd = connection();
+
+// Fonction qui utilise les informations de la BDD
+function menuDeroulant($database) {
+
+  $menu = "";
+  $reqDrinkName = $database->query('SELECT nomboisson FROM boisson');
+
+  // affiche chaque entrée une à une
+  while ($tabDrinkName = $reqDrinkName->fetch())
+  {
+    // ajoute une balise de champs de menu déroulant avec les données de la BDD externe
+    $menu .= "<option>".$tabDrinkName["nomboisson"]."</option>";
+  }
+  return $menu;
+  $reqDrinkName->closeCursor();
+}
+  
+function afficheRecette($database, $choixBoisson, $choixSucres) {
+  
+  $i=0;
+  $recetteFinale = "";
+  
+  // Si la connexion est ok, récupère la liste des boissons 
+  $reponseBddMachineacafe = $database->prepare ('
+    SELECT  nomboisson, nomingredients, qteboisson
+    FROM boisson_ingredients
+    INNER JOIN boisson ON boisson.codeboisson = boisson_ingredients.boisson_codeboisson
+    INNER JOIN ingredients ON ingredients.codeingredients = boisson_ingredients.ingredients_codeingredients
+    WHERE nomboisson = ?
+  '); // Identique à WHERE nomboisson = :nomboisson
+
+  $reponseBddMachineacafe->execute(array($choixBoisson)); // Avec le ? s'écrit : execute(array($_POST["choixBoisson"]));
+  
+  // Affiche la valeur nomboisson selectionnée
+  // var_dump(array($choixBoisson)); 
+
+  // affiche chaque entrée une à une
+  while ($tabDonnees = $reponseBddMachineacafe->fetch())
+  {
+    if ($i==0)
+    {
+      $i=1;// Affiche le nom de la boisson une seule fois
+      $recetteFinale .= "<p>"." Vous avez commandé un ".$tabDonnees["nomboisson"]."</p>"."Dont la recette est"."<br>";
+    }
+      $recetteFinale .= $tabDonnees["nomingredients"]." ".$tabDonnees["qteboisson"]." dose(s) <br>";
+    
+  }
+
+  if ($choixSucres == 1)  {
+    $recetteFinale .=  "avec ".$choixSucres . " sucre" ."<br>";
+  } else if ($choixSucres > 1) {
+    $recetteFinale .=  "avec ".$choixSucres . " sucres" ."<br>";
+  } else if ($choixSucres == 0) {
+    $recetteFinale .=  "Sans sucre" ."<br>";
+  }
+
+  return $recetteFinale;
+  $reponseBddMachineacafe->closeCursor(); // Termine le traitement de la requête
+}
+
+function ajouterSucre($recetteTab, $nbSucres) {
+  if ($nbSucres == 1) {
+    array_push($recetteTab, " Sucre x " . $nbSucres);
+  } else if ($nbSucres > 1) {
+    array_push($recetteTab, " Sucres x " . $nbSucres);
+  } else if ($nbSucres == 0) {
+    array_push($recetteTab, " Sans sucre");
+  }
+
+  return $recetteTab;
+}
+
+?>
---
+++ MachineACafe/machineACafe.php
@@ -0,0 +1,73 @@
+<?php
+	//lien vers les fonctions php
+	include "assets/_php/fonctions.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Machine à café en php</title>
+	<!-- liens vers les librairies jquery et bootstrap-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="assets/_css/machineACafe.css">
+
+</head>
+<body>
+	<div class="mainContener">
+		<h1>Ma Machine à café en PHP</h1>
+		
+		<div id="afficheurInfo" class="date">
+			Today is <?= $date?> <!-- Insertion de la date du jour en php -->
+			il est <?= $heure?> <!-- Insertion de l'heure en php -->
+		    heures et <?=$minutes?> minutes <!-- Insertion des minutes en php -->
+		</div>
+
+		<div class="blocInfos">
+			<form method="post" action="machineACafe.php">
+				<select name="choixBoisson">
+					<option>Choisissez votre boisson</option>
+					<?= menuDeroulant($mabdd)?>
+				</select>
+				<input type="number" min="0" max="5" name="choixSucre" placeholder="Combien de sucres ?"/>
+				<input type="submit" value="Valider"/>
+			</form>
+			<p>
+			
+			</p>
+		</div>
+
+		
+		<div class="blocInfos">
+
+		<?php
+
+			if (isset($_POST["choixBoisson"]) AND isset($_POST["choixSucre"])) 
+			{
+			  echo afficheRecette($mabdd,$_POST["choixBoisson"],$_POST["choixSucre"]);
+			} 
+			else
+			{
+			  echo $messageAttente;
+			} 
+
+		?>
+
+		</div>
+			
+		
+	   	<!-- <div id="pieces">
+	       	<img id="btnCinqCts" class="piece" alt="0.05" src="images/5_cts.png">
+	        <img id="btnDixCts" class="piece" alt="0.10" src="images/10_cts.png">
+	        <img id="btnVingtCts" class="piece" alt="0.20" src="images/20_cts.png">
+	        <img id="btnCinquanteCts" class="piece" alt="0.50" src="images/50_cts.png">
+	        <img id="btnUnEuro" class="piece" alt="1.00" src="images/1_euros.png">
+	        <img id="btnDeuxEuros" class="piece" alt="2.00" src="images/2_euros.png">
+	    </div> -->
+		    <!-- <div id="afficheurMonnaie">
+		        <div id="monnayeur">Crédit : 0.00 €</div>
+			</div>
+			<div id="btnResetCoin"><button>Reset Coin</button></div> -->
+	</div>
+</body>
+</html>
